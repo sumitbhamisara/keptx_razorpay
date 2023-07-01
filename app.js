86d2ed2f204d423523bc4ffee1125a6a7f4ec82a
syed asim <aasimshah8110@gmail.com>
2023-07-02 01:28:35 +0500

    3sd

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ app.post("/rn/api/create_qr", verifyToken, async (req, res) => {
         name: name,
         usage: "multiple_use",
         fixed_amount: false,
-        description : "wallet_2333id"
+        description 
     }
     const pipeline = util.promisify(require('stream').pipeline);
 
